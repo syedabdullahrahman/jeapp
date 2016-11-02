a3816c759e011fae6b38a598921870f0b348d49d
Krzysiek <viruss.snk@gmail.com>
2016-11-02 17:51:47 +0100

    initial commmit

---
+++ src/main/java/jeapp/ejb/HelloBean.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jeapp.ejb;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author Viruss
+ */
+@Named(value = "helloBean")
+@Dependent
+public class HelloBean {
+private String helloMessage;
+    /**
+     * Creates a new instance of HelloBean
+     */
+    public HelloBean() {
+        this.helloMessage = "Hello from HelloBean!";
+    }
+
+    /**
+     * @return the helloMessage
+     */
+    public String getHelloMessage() {
+        return helloMessage;
+    }
+
+    /**
+     * @param helloMessage the helloMessage to set
+     */
+    public void setHelloMessage(String helloMessage) {
+        this.helloMessage = helloMessage;
+    }
+    
+    
+    
+}
